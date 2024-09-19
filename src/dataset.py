be9c8fb3d7c3b9136238bca4f4c7441a386ef322
Fernando Fincatti <fernandocfbf@al.insper.edu.com.br>
2024-09-18 23:01:18 -0300

    feat: setting up the training function inside the FoodSegmentation class. Still need to setup cuda to train the model

--- src/dataset.py
+++ src/dataset.py
@@ -1,6 +1,8 @@
 import os
 import cv2
 from torch.utils.data import Dataset
+import torchvision.transforms as T
+from PIL import Image
 
 class FoodSegDataset(Dataset):
     def __init__(self, image_dir:str, mask_dir:str, mode:str="train"):
@@ -9,6 +11,18 @@ class FoodSegDataset(Dataset):
         self.images = os.listdir(self.image_dir)
         self.annotations = os.listdir(self.mask_dir)
         self.mode = mode
+        self.image_size = (520,520)
+        self.transform_image = T.Compose([
+            T.ToPILImage(),
+            T.Resize(self.image_size),
+            T.ToTensor(),
+            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+        self.transform_mask = T.Compose([
+            T.ToPILImage(),
+            T.Resize(self.image_size, interpolation=Image.NEAREST),
+            T.ToTensor(),
+        ])
 
     def __len__(self):
         return len(self.images)
@@ -19,4 +33,6 @@ class FoodSegDataset(Dataset):
         image = cv2.imread(img_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+        image = self.transform_image(image)
+        mask = self.transform_mask(mask)
         return image, mask
--- src/model.py
+++ src/model.py
@@ -1,11 +1,11 @@
+from tqdm import tqdm
 import matplotlib.pyplot as plt
-import numpy as np
-import cv2
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision.models.segmentation import deeplabv3_resnet50
 import torchvision.transforms as T
+from PIL import Image
 
 class FoodSegmentation:
     def __init__(self, num_classes:int, device:str="cuda"):
@@ -14,37 +14,54 @@ class FoodSegmentation:
         self.model = self._build_model()
         self.model.to(self.device)
         self.model.eval()
-        self.transform = T.Compose([
+
+    def _image_transform(self, image):
+        return T.Compose([
             T.ToPILImage(),
-            T.Resize((520, 520)),  # Resize to the required input size
+            T.Resize((520, 520)),
             T.ToTensor(),
             T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
+        ])(image)
+    
+    def _mask_transform(self, mask):
+        return T.Compose([
+            T.Resize((520, 520), interpolation=Image.NEAREST),
+            T.ToTensor(),
+        ])(mask)
 
     def _build_model(self):
         model = deeplabv3_resnet50(pretrained=True, num_classes=self.num_classes)
-        #model.classifier[4] = nn.Conv2d(256, self.num_classes, kernel_size=(1,1), stride=(1,1))
+        for param in model.backbone.parameters():
+            param.requires_grad = False
+        model.classifier[4] = nn.Conv2d(256, self.num_classes, kernel_size=(1,1))
         return model
 
     def set_optimizer(self, learning_rate:float=1e-3):
-        self.otimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
 
     def set_loss_function(self):
         self.criterion = nn.CrossEntropyLoss()
 
-    def train(self, x, y, epochs=10, batch_size=32):
+    def train(self, train_loader, epochs=10):
+        self.model.train()
         for epoch in range(epochs):
             running_loss = 0.0
-            for i, (inputs, targets) in enumerate(zip(x, y)):
-                inputs = inputs.to(self.device)
-                targets = targets.to(self.device)
-                self.optimizer.zero_grad()
-                outputs = self.model(inputs)
-                loss = self.criterion(outputs, targets)
-                loss.backward()
-                self.optimizer.step()
-                running_loss += loss.item()
-                print(f"Epoch {epoch+1}/{epochs}, Loss: {running_loss / len(x)}")
+            with tqdm(train_loader, unit="batch") as tepoch:
+                tepoch.set_description(f"Epoch [{epoch + 1}/{epochs}]")
+                for images, masks in train_loader:
+
+                    images = images.to(self.device)
+                    masks = masks.to(self.device).long()
+                    outputs = self.model(images)["out"]
+                    loss = self.criterion(outputs, masks.squeeze(1))
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    self.optimizer.step()
+                    running_loss += loss.item()
+                    tepoch.set_postfix(loss=loss.item())
+            epoch_loss = running_loss / len(train_loader)
+            print(f'Epoch [{epoch + 1}/{epochs}], Loss: {epoch_loss:.4f}')
+        print('Training complete')
 
     def plot_results(self, original_image, predicted_mask):
         plt.figure(figsize=(10, 5))
@@ -61,7 +78,7 @@ class FoodSegmentation:
 
     def visualize_prediction(self, image):
         original_image = image.copy()
-        tensor_image = self.transform(image).unsqueeze(0)
+        tensor_image = self._image_transform(image).unsqueeze(0)
         tensor_image = tensor_image.to(self.device)
         with torch.no_grad():
             outputs = self.model(tensor_image)["out"]
