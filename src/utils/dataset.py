b262a8c9eafc26601aea772081f9dc4fdde3f75a
Fernando Fincatti <fernandocfbf@al.insper.edu.com.br>
2025-03-29 15:12:37 -0300

    feat: adding visualization with label into exploratory_analysis.ipynb

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -1,6 +1,40 @@
+import pandas as pd
 import numpy as np
 from PIL import Image
 import io
+from datasets import load_dataset
+
+def load_foodseg103(type:str="all") -> pd.DataFrame:
+    """
+    Loads the FoodSeg103 dataset as a Pandas DataFrame.
+
+    Parameters
+    ----------
+    type : str, optional
+        Specifies which subset of the dataset to load. Options:
+        - "train": Loads only the training set.
+        - "validation": Loads only the validation set.
+        - "all" (default): Loads both training and validation sets, concatenated into a single DataFrame.
+    
+    Returns
+    -------
+    pd.DataFrame
+        A Pandas DataFrame containing the requested subset of the FoodSeg103 dataset.
+    
+    Raises
+    ------
+    ValueError
+        If an invalid `type` is provided.
+    """
+    valid_types = {"train", "validation", "all"}
+    if type not in valid_types:
+        raise ValueError(f"Invalid type '{type}'. Expected one of: {valid_types}")
+    food_seg_103 = load_dataset("EduardoPacheco/FoodSeg103")
+    if type == "all":
+        train_df = food_seg_103["train"].to_pandas()
+        validation_df = food_seg_103["validation"].to_pandas()
+        return pd.concat([train_df, validation_df])
+    return food_seg_103[type].to_pandas()
 
 def decode_image_from_bytes(byte_data: dict) -> np.ndarray:
     """
--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -1,15 +1,19 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import cv2
+import numpy as np
+
+from src.constants.category_id import CATEGORY_ID
 
 def show_image_preview(dataset: pd.DataFrame, image_id:int) -> None:
     """
-    Displays the original image and its corresponding annotation mask side by side.
+    Displays the original image, its annotation mask, and a labeled version with class names.
 
     Parameters
     ----------
     dataset : pd.DataFrame
-        DataFrame containing images and their annotation masks. 
-        It must include the columns "original_image" and "annotation_mask".
+        DataFrame containing images and their corresponding annotation masks.
+        Must include the columns "image_decoded" (original image) and "annotation_mask" (segmentation mask).
     
     image_id : int
         Index of the image in the DataFrame to be displayed.
@@ -18,15 +22,33 @@ def show_image_preview(dataset: pd.DataFrame, image_id:int) -> None:
     -------
     None
         The function displays the images using Matplotlib but does not return any values.
-    """
-    image_information = dataset.loc[image_id]
-    original_image = image_information["original_image"]
-    annotation_mask = image_information["annotation_mask"]
     
-    _, axs = plt.subplots(1, 2, figsize=(15, 5))
-    axs[0].imshow(original_image)
+    """
+    fig, axs = plt.subplots(1, 3, figsize=(12, 3))
     axs[0].set_title('Original Image')
+    axs[1].set_title('Annotation Mask')
+    axs[2].set_title('Original Image with Labels')
+    axs[0].axis('off')
+    axs[1].axis('off')
+    axs[2].axis('off')
+    unique_labels = np.unique(dataset.loc[image_id, 'annotation_mask'])
+    example_info = dataset.loc[image_id]
+    original_image = example_info["image_decoded"]
+    annotation_mask = example_info["annotation_mask"]
+    label_array = np.array(annotation_mask)
+    axs[0].imshow(original_image)
     axs[0].axis('off')
     axs[1].imshow(annotation_mask, cmap="tab20")
-    axs[1].set_title("Annotation Mask")
     axs[1].axis("off")
+    axs[2].imshow(annotation_mask, cmap="tab20")
+    axs[2].axis("off")
+    for lbl in unique_labels:
+        if lbl != 0: #ignore background
+            mask = (label_array == lbl).astype(np.uint8)
+            M = cv2.moments(mask)
+            if M["m00"] != 0:  # avoid division by zero
+                cx = int(M["m10"] / M["m00"])  # centroid x
+                cy = int(M["m01"] / M["m00"])  # centroid y
+                class_name = f"{CATEGORY_ID.get(lbl, 'Unknown')}"  # Get class name
+                axs[2].text(cx, cy, class_name, color="white", fontsize=12, ha="center", va="center", 
+                    bbox=dict(facecolor='black', alpha=0.5, edgecolor='none'))
