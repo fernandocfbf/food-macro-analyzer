e9a9c942886785d556496220e2d11ee93313b5e0
Fernando Fincatti <fernandocfbf@al.insper.edu.com.br>
2025-04-14 22:26:51 -0300

    feat: adding pytorch lightning model training

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -29,7 +29,7 @@ def load_foodseg103(type:str="all", sample_size:int=None) -> pd.DataFrame:
     valid_types = {"train", "validation", "all"}
     if type not in valid_types:
         raise ValueError(f"Invalid type '{type}'. Expected one of: {valid_types}")
-    food_seg_103 = load_dataset("EduardoPacheco/FoodSeg103", )
+    food_seg_103 = load_dataset("EduardoPacheco/FoodSeg103")
     if type == "all":
         train_df = food_seg_103["train"].to_pandas()
         validation_df = food_seg_103["validation"].to_pandas()
@@ -54,5 +54,5 @@ def decode_image_from_bytes(byte_data: dict) -> np.ndarray:
         A NumPy array representing the decoded image.
     """
     image_bytes = byte_data["bytes"]
-    image = Image.open(io.BytesIO(image_bytes))
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
     return np.array(image)
